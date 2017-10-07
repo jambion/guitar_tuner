4a3a6d31a2819041b2a01e3c3b5183587c87211d
JXA <Ambion_J@yahoo.com>
2017-10-07 15:47:46 -0700

    Event delegated and modularized guitar tuner functions

--- tuner.js
+++ tuner.js
@@ -1,43 +1,35 @@
 $(document).ready(function(){
 
-	console.log("Hello World!");
 
-  var lowEAudio = $("#lowEmp3");
-	$("#lowE").on("click", function(){
-		console.log("Clicked on low E");
-		var sound = $("#audioLowE")[0];
+	var playClip = function(clipData) {
+		var sound = $(clipData.sound)[0];
 		sound.play();
+	}
+
+
+	$("#lowE").on("click", {sound: "#audioLowE"}, function(event){
+		playClip(event.data);
 	});
 
-	$("#A").on("click", function(){
-		console.log("Clicked on A");
-		var sound = $("#audioA")[0];
-		sound.play();
+	$("#A").on("click", {sound: "#audioA"}, function(event){
+		playClip(event.data);
+	});
+	$("#D").on("click", {sound: "#audioD"}, function(event){
+		playClip(event.data);
 	});
 
-	$("#D").on("click", function(){
-		console.log("Clicked on D");
-		var sound = $("#audioD")[0];
-		sound.play();
+	$("#G").on("click", {sound: "#audioG"}, function(event){
+		playClip(event.data);
 	});
 
-	$("#G").on("click", function(){
-		console.log("Clicked on G");
-		var sound = $("#audioG")[0];
-		sound.play();
+	$("#B").on("click", {sound: "#audioB"}, function(event){
+		playClip(event.data);
 	});
 
-	$("#B").on("click", function(){
-		console.log("Clicked on B");
-		var sound = $("#audioB")[0];
-		sound.play();
+	$("#highE").on("click", {sound: "#audioHighE"}, function(event){
+		playClip(event.data);
 	});
 
-	$("#highE").on("click", function(){
-		console.log("Clicked on high E");
-		var sound = $("#audioHighE")[0];
-		sound.play();
-	});
 
 	$("#strings > div").mouseenter(function(){
 		$(this).fadeTo(100, 0.7);
