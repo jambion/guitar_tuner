21712f7c09bd250a4022ef3a6c4dcca0831cc00e
JXA <Ambion_J@yahoo.com>
2017-10-07 14:32:05 -0700

    Added The Turtle Descriptions

--- tuner.js
+++ tuner.js
@@ -50,17 +50,24 @@ $(document).ready(function(){
 	});
 
 	const turtleMask = ["orange", "purple", "blue", "red"];
+	const turtleDescription = [
+	"Mikey is the jokester. Slice of pizza in hand and shouts of 'COWABUNGA!', Mikey is a turtle that's always ready for a good time. He had tons of energy, and is playful and fun. He's the life of the party.",
+	"Donny is the thinker. Every team needs a brain, and for the Turtles, that's Donny. He is meticulous and thinks problems through before making decisions. Though he can hold his own in a fight, he is more inclined to towards gadgets, mechanical devices, and mathematics.",
+	"Leo is the heart of the team. Loyal, brave, and responsible, Leonardo is the leader of the team, but not by choice. His mentor Splinter chose him to be the leader of the group because he knew that Leonardo could be trusted to always act in the best interests of the entire team.",
+	"Raphael is fearless. Though not the leader of the group, Raphael is definitely the group's alpha. On a good day, you might think of him as brave, fearless, and resourceful. On a bad day, he's bossy, impulsive, and hot headed."]
 
 	let maskNum = 0;
 
 	$(".changeTurtle").click(function(){
 		$(".mask").css({
-			"background": turtleMask[maskNum % 4]
+			"background": turtleMask[maskNum % turtleMask.length]
 		});
+		$("#turtleInfo").html(turtleDescription[maskNum]);
 		maskNum++;
 	});
 
 	$(".pizzaTurtle").click(function(){
 		$(".mask").css("background", "url('images/za.jpg')");
+		$("#turtleInfo").html("PIZZA TIME!");
 	})
 });
